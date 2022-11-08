a8ce9e412d91069dd4ab6cbeee6dfc4a64587a22
Kyle Pidgeon <kyle.pidgeon@stfc.ac.uk>
2022-11-08 10:17:18 +0000

    Add function for calculating std dev

--- inflammation/models.py
+++ inflammation/models.py
@@ -36,3 +36,11 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+
+def daily_std(data):
+    """Calculate the daily standard deviation of a 2D inflammation data array
+
+    :param data: numpy.ndarray
+    :returns: np.ndarray
+    """
+    return np.std(data, axis=0)
